67d6d9ae998fa37eb4f2ad92f52491f156f449ba
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-12-02 15:38:01 +0700

    feat: add loader component with style

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import "../styles/loader.css";
+
+function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="loader-spinner"></div>
+    </div>
+  );
+}
+
+export default Loader;
