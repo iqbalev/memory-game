01ec0d6e27b58a6dda8c6ea59df70d237d686886
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-12-04 10:54:18 +0700

    chore: remove unnecessary comment from capitalize

--- src/utils/capitalize.js
+++ src/utils/capitalize.js
@@ -3,7 +3,6 @@ function capitalize(text) {
     .split(" ")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join(" ");
-  //   text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
   return capitalizedText;
 }
 
